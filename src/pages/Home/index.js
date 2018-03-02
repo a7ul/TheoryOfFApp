b903d85aae105a957da9f38e81b618e417481629
Atul R <atulanand94@gmail.com>
2018-03-02 23:57:26 +0530

    Adding basic perf

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,12 +10,6 @@ import { Text, View } from 'react-native';
 export default class Home extends Component {
   static navigationOptions = {
     title: '🖕',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-      fontSize: 20,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
   };
   render() {
     return (
